b20e0291da3c679ab17ae678579791610868e5ed
zanny31 <jianb0331@naver.com>
2025-01-19 20:58:28 +0900

    [Chapter02-02] 프로덕션 코드와 테스트 코드 분리

---
+++ chapter02/src/calculator02/Calculator02.java
@@ -0,0 +1,20 @@
+package calculator02;
+
+public class Calculator02 {
+	int add(int i, int y) {
+		return i+y;
+	}
+	
+	int substract(int i, int y) {
+		return i-y;
+	}
+	
+	int multiply(int i, int y) {
+		return i*y;
+	}
+	
+	int divide(int i, int y) {
+		return i/y;
+	}
+	
+}
